dfffd4ef3d7cec60ae3aed293ae5dcdab016103a
Marc Thiongo <marcthuo@gmail.com>
2019-04-19 13:43:02 -0400

    react app up and running, no jokes yet, users done

---
+++ authenticate/src/jokes/jokes_list.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Auth from '../auth/Auth';
+import styled from 'styled-components';
+
+const List = styled.div`
+    color: red;
+    font-weight: bold;
+`;
+
+class Jokes extends Component {
+    state = {
+        jokes:[]
+    };
+
+    render() {
+        console.log('You are in the Jokes page!');
+        return (
+            <List>
+                <h2>List of Jokes</h2>
+                <ul>
+                    {this.state.jokes.map(user => (
+                        <li key={user.id}>{user.username}</li>
+                    ))}
+                </ul>
+            </List>
+        )
+    }
+ componentDidMount() {
+      axios.get('/api/jokes').then(res => {
+            this.setState({ jokes: res.data })
+        })   
+    }    
+}
+
+export default Auth(Jokes);
---
+++ authenticate/src/users/users_list.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Auth from '../auth/Auth';
+import styled from 'styled-components';
+
+const List = styled.div`
+    color: red;
+    font-weight: bold;
+`;
+
+class Users extends Component {
+    state = {
+        users:[]
+    };
+
+    render() {
+        console.log('You are in the Users page!');
+        return (
+            <List>
+                <h2>List of Users</h2>
+                <ul>
+                    {this.state.users.map(user => (
+                        <li key={user.id}>{user.username}</li>
+                    ))}
+                </ul>
+            </List>
+        )
+    }
+ componentDidMount() {
+      axios.get('/api/users').then(res => {
+            this.setState({ users: res.data })
+        })   
+    }    
+}
+
+export default Auth(Users);
